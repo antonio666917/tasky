5c9b00a2bdbd4f8135c79c4a1d8b4f921a55999e
Antonio Rodriguez <antoniwan@gmail.com>
2018-05-25 18:25:40 -0400

    fix a typo

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import Tasky from "../components/Tasky/Tasky";
+import Tasky from "../components/Tasky";
 
 const StyledHome = styled.main``;
 
